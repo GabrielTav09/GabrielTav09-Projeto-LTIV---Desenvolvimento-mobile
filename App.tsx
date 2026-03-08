4c9b26aa31ed5b9174ed42d869b260523d4412a6
Gabriel Silva Tavares <gabriesilvaestudo41@gmail.com>
2026-03-08 14:31:07 -0300

    Meu primeiro app Expo para a aula

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>helo word! {"\n"} teste
+      </Text>
+      <Text>Descrição detalhada logo abaixo.</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
